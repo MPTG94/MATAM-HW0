d0b5e1bf58bfc3553484d7fe7dcdb1f5342ff968
Mor Paz <1912.mor@gmail.com>
2019-10-25 20:23:03 +0300

    finish ex1 of the task

--- mtm_tot/mtm_tot/mtm_tot.c
+++ mtm_tot/mtm_tot/mtm_tot.c
@@ -1,6 +1,6 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include <math.h>
 
 #pragma warning(disable : 4996)
 void printEnterInputSizeMessage();
@@ -9,37 +9,59 @@ void printInvalidSizeMessage();
 void printInvalidNumberMessage();
 void printFoundExponentMessage(int n, int exponent);
 void printTotalExponentSum(int n);
-bool isPowerOfTwo(int n);
-int getExponentOfTwo(int n);
+int getExponentInBaseTwo(int x);
 
 int main()
 {
-	int input_number = 0;
+	
+	int input_size = 0;
 	int exponent_sum = 0;
 	printEnterInputSizeMessage();
-	if (scanf("%d", &input_number) == 0 || input_number <= 0)
+	if (scanf("%d", &input_size) == 0 || input_size <= 0)
 	{
-		printInvalidNumberMessage();
+		// Size is illegal.
+		printInvalidSizeMessage();
 	}
 	else
 	{
-		for (int i = 0; i < input_number; i++)
+		int* numbers_array = (int*)malloc(input_size * sizeof(int));
+		int count = 0;
+		// Initialize array to store the input numbers.
+		if (numbers_array == NULL)
+		{
+			return 0;
+		}
+		for (int i = 0; i < input_size; i++)
+		{
+			numbers_array[i] = 0;
+		}
+		printEnterNumbersMessage();
+		for (int i = 0; i < input_size; i++)
 		{
 			int temp = 0;
-			printEnterNumbersMessage();
-			if (scanf("%d", &temp) == 0 || input_number <= 0)
+			if (scanf("%d", &temp) == 0)
 			{
+				// Number is illegal.
 				printInvalidNumberMessage();
+				free(numbers_array);
 				return 0;
 			}
-			if (isPowerOfTwo(temp))
+			numbers_array[i] = temp;
+			count++;
+			
+		}
+		for (int i = 0; i <= count; i++)
+		{
+			// Calculating the base2 exponent of each input number and adding to sum.
+			int exponent = getExponentInBaseTwo(numbers_array[i]);
+			if (exponent != -1)
 			{
-				int exponent = getExponentOfTwo(temp);
-				printFoundExponentMessage(temp, exponent);
+				printFoundExponentMessage(numbers_array[i], exponent);
 				exponent_sum += exponent;
 			}
 		}
 		printTotalExponentSum(exponent_sum);
+		free(numbers_array);
 	}
 	return 0;
 }
@@ -74,17 +96,23 @@ void printTotalExponentSum(int n)
 	printf("Total exponent sum is %d\n", n);
 }
 
-bool isPowerOfTwo(int n)
+int getExponentInBaseTwo(int n)
 {
+	int count = 0;
 	if (n == 0)
 	{
-		return false;
+		return -1;
 	}
 
-	return (ceil(log2(n)) == floor(log2(n)));
-}
-
-int getExponentOfTwo(int n)
-{
-	return log2(n);
+	// Divide number until the result is 1.
+	while (n != 1)
+	{
+		// Check if the number is divisable by 2.
+		if (n % 2 != 0)
+			return -1;
+		n /= 2;
+		// Count each division to calculate the exponent.
+		count++;
+	}
+	return count;
 }
