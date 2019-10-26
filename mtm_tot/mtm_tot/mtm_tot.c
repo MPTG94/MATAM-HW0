b0d9438ddc39cdfe3ad4555d34fae550c6bdca54
Mor Paz <1912.mor@gmail.com>
2019-10-26 16:53:54 +0300

    update styling for mtm_tot

--- mtm_tot/mtm_tot/mtm_tot.c
+++ mtm_tot/mtm_tot/mtm_tot.c
@@ -23,40 +23,31 @@ void printFoundExponentMessage(int n, int exponent);
 void printTotalExponentSum(int n);
 
 // Calculated the base 2 exponent of an input number.
-int getExponentInBaseTwo(int x);
+int calculateExponentInBaseTwo(int x);
 
 //==============================================================================
 
-int main()
-{
-	
+int main() {
 	int input_size = 0;
 	int exponent_sum = 0;
 	printEnterInputSizeMessage();
-	if (scanf("%d", &input_size) == 0 || input_size <= 0)
-	{
+	if (scanf("%d", &input_size) == 0 || input_size <= 0) {
 		// Size is illegal.
 		printInvalidSizeMessage();
-	}
-	else
-	{
+	}	else {
 		int* numbers_array = (int*)malloc(input_size * sizeof(int));
 		int count = 0;
 		// Initialize array to store the input numbers.
-		if (numbers_array == NULL)
-		{
+		if (numbers_array == NULL) {
 			return 0;
 		}
-		for (int i = 0; i < input_size; i++)
-		{
+		for (int i = 0; i < input_size; i++) {
 			numbers_array[i] = 0;
 		}
 		printEnterNumbersMessage();
-		for (int i = 0; i < input_size; i++)
-		{
+		for (int i = 0; i < input_size; i++) {
 			int temp = 0;
-			if (scanf("%d", &temp) == 0)
-			{
+			if (scanf("%d", &temp) == 0) {
 				// Number is illegal.
 				printInvalidNumberMessage();
 				free(numbers_array);
@@ -64,14 +55,12 @@ int main()
 			}
 			numbers_array[i] = temp;
 			count++;
-			
+
 		}
-		for (int i = 0; i <= count; i++)
-		{
+		for (int i = 0; i <= count; i++) {
 			// Calculating the base2 exponent of each input number and adding to sum.
-			int exponent = getExponentInBaseTwo(numbers_array[i]);
-			if (exponent != -1)
-			{
+			int exponent = calculateExponentInBaseTwo(numbers_array[i]);
+			if (exponent != -1) {
 				printFoundExponentMessage(numbers_array[i], exponent);
 				exponent_sum += exponent;
 			}
@@ -82,50 +71,43 @@ int main()
 	return 0;
 }
 
-void printEnterInputSizeMessage()
-{
+void printEnterInputSizeMessage() {
 	printf("Enter size of input:\n");
 }
 
-void printEnterNumbersMessage()
-{
+void printEnterNumbersMessage() {
 	printf("Enter numbers:\n");
 }
 
-void printInvalidSizeMessage()
-{
+void printInvalidSizeMessage() {
 	printf("Invalid size\n");
 }
 
-void printInvalidNumberMessage()
-{
+void printInvalidNumberMessage() {
 	printf("Invalid number\n");
 }
 
-void printFoundExponentMessage(int n, int exponent)
-{
+void printFoundExponentMessage(int n, int exponent) {
 	printf("The number %d is a power of 2: %d = 2^%d\n", n, n, exponent);
 }
 
-void printTotalExponentSum(int n)
-{
+void printTotalExponentSum(int n) {
 	printf("Total exponent sum is %d\n", n);
 }
 
-int getExponentInBaseTwo(int n)
-{
+int calculateExponentInBaseTwo(int n) {
 	int count = 0;
-	if (n == 0)
-	{
+	if (n == 0) {
 		return -1;
 	}
 
 	// Divide number until the result is 1.
-	while (n != 1)
-	{
+	while (n != 1) {
 		// Check if the number is divisable by 2.
-		if (n % 2 != 0)
+		if (n % 2 != 0) {
 			return -1;
+		}
+
 		n /= 2;
 		// Count each division to calculate the exponent.
 		count++;
